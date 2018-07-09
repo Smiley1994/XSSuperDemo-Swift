2bc5346ce4dba93de86bc0706284113c3691a3dc
songf <smileysun1994@gmail.com>
2018-07-09 10:28:28 +0800

    update

--- XSSuperDemo-Swfit/ViewController.swift
+++ XSSuperDemo-Swfit/ViewController.swift
@@ -8,11 +8,16 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+//        self.view.backgroundColor = UIColor.randomColor
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
