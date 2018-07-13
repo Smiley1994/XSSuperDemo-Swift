2c7712a2d8c431db6f2b2f9ee659260fd96857b0
SunXiaoSong <sunxiaosong@ann9.com>
2018-07-13 18:29:36 +0800

    update

---
+++ Carthage/Checkouts/XSCommon/Sample/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Sample
+//
+//  Created by Good_Morning_ on 2018/7/4.
+//  Copyright © 2018年 GoodMorning. All rights reserved.
+//
+
+import UIKit
+import XSCommon
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.randomColor
+        
+        let button = XSIQiYiPlayButton(frame: CGRect(x: 100, y: 100, width: 100, height: 100), status: .IQiYiPlayStatusPlay, color: UIColor.randomColor)
+        view.addSubview(button)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
